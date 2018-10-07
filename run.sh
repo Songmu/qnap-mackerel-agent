b2b31f9acc20f84824c3be509be8d80e6b3618e5
Songmu <y.songmu@gmail.com>
2018-10-07 17:48:59 +0900

    adjust

--- run.sh
+++ run.sh
@@ -6,23 +6,25 @@ PROG=/share/MD0_DATA/.$NAME/bin/$NAME
 CONF=/share/MD0_DATA/.$NAME/$NAME.conf
 
 case "$1" in
-    start)
-        mypid=$(/bin/pidof $NAME)
-        if [ ! -z $mypid ]; then
-                exit 1
-        else
-            echo -n "Starting $NAME: "
-            $DAEMON_MGR $NAME start "$PROG --conf=$CONF &amp;"
-            exit 0
-        fi
-        ;;
-    stop)
-        echo -n "Stopping $NAME: "
-        $DAEMON_MGR $NAME stop "$PROG"
-        echo
-        ;;
-    *)
-        echo "Usage: $1 start|stop"
-        exit 1
-        ;;
+  start)
+    mypid=$(/bin/pidof $NAME)
+    if [ ! -z "$mypid" ]; then
+      exit 1
+    else
+      echo "Starting $NAME: "
+      echo
+      $DAEMON_MGR $NAME start "$PROG --conf=$CONF &"
+      exit 0
+    fi
+    ;;
+  stop)
+    echo "Stopping $NAME: "
+    echo
+    $DAEMON_MGR $NAME stop "$PROG"
+    echo
+    ;;
+  *)
+    echo "Usage: $1 start|stop"
+    exit 1
+    ;;
 esac
