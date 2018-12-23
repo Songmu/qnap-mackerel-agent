9eb0e879c1bf9a83c148ff53797f0cac6f70aec4
Songmu <y.songmu@gmail.com>
2018-12-23 15:19:02 +0900

    update installing mackerel-agent.conf process

--- cmd/mkrinst/agent.go
+++ cmd/mkrinst/agent.go
@@ -1,5 +1,37 @@
 package main
 
-func doAgentConf(argv []string) error {
-	return nil
+import (
+	"fmt"
+	"os"
+	"text/template"
+)
+
+func doAgentConf(argv []string) (err error) {
+	if len(argv) < 2 {
+		return fmt.Errorf("install_path or mackerel_api_key aren't specified")
+	}
+	installPath := argv[0]
+	mackerelAPIKey := argv[1]
+	confFile := "./mackerel-agent.conf"
+	confTmpl := "./mackerel-agent.conf.tmpl"
+
+	tmpl, err := template.ParseFiles(confTmpl)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.OpenFile(confFile, os.O_CREATE|os.O_EXCL, 0600)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if e := f.Close(); e != nil {
+			err = e
+		}
+	}()
+
+	return tmpl.Execute(f, struct{ InstallPath, MackerelAPIKey string }{
+		InstallPath:    installPath,
+		MackerelAPIKey: mackerelAPIKey,
+	})
 }
--- install.sh
+++ install.sh
@@ -9,12 +9,10 @@ cp -rf "$(dirname "$0")/" "$install_path"
 
 cd $install_path
 if [ ! -f ./mackerel-agent.conf ]; then
-  bin/mkrinst agentconf "$install_path"
-  install -m 0600 mackerel-agent.conf.sample mackerel-agent.conf
   while [ "$MACKEREL_APIKEY" = "" ]; do
     read -rp "input Mackerel APIKEY: " MACKEREL_APIKEY
   done
-  bin/mackerel-agent init "-apikey=$MACKEREL_APIKEY" -conf ./mackerel-agent.conf
+  bin/mkrinst agentconf "$install_path" "$MACKEREL_APIKEY"
 fi
 
 bin/mkrinst qpkgconf "$install_path"
